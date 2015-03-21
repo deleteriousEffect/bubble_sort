80f9cf2dd104e66068b1954514cd9157c7672877
Hayley Swimelar <hswimelar@gmail.com>
2015-03-20 21:14:28 -0700

    add bubble_sort_by method

--- bubble_sort.rb
+++ bubble_sort.rb
@@ -23,10 +23,39 @@ def bubble_sort(array)
   array
 end
 
-test_arr = Array.new(2000).map { rand(-100000..100000) }
-
-print bubble_sort(test_arr)
-
 # Bubble sort which takes a block to determine the sorting method.
 def bubble_sort_by(array)
+  number_of_passes = 0
+  loop do
+    swapped = false
+    # The element we are deciding whether to swap with the next element or not.
+    sorting_el = 0
+    # For n passes through an array, the last n elements will be sorted.
+    last_unsorted_index = array.length - (1 + number_of_passes)
+    while last_unsorted_index > 0
+      # If the result of the block is negative swap element positions.
+      if (yield array[sorting_el], array[sorting_el + 1]) < 0
+        array.insert(sorting_el + 1, array.delete_at(sorting_el))
+        swapped = true
+      end
+      sorting_el += 1
+      last_unsorted_index -= 1
+    end
+    number_of_passes += 1
+    # If no elements were swapped, the array is sorted.
+    break if swapped == false
+  end
+  array
 end
+
+test_arr = Array.new(2000).map { rand(-100000..100000) }
+
+#print bubble_sort(test_arr)
+
+print(bubble_sort_by(['hi', 'hello', 'hey', 'a', 'long sting']) do |left, right|
+  right.length - left.length
+end)
+
+print(bubble_sort_by(['hi', 'hello', 'hey', 'a', 'long sting']) do |left, right|
+  left.length - right.length
+end)
